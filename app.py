21babb7863f360a993271d96042b076bc067904e
Dajaalmie <dajaalmie@gmail.com>
2026-04-02 21:26:52 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,10 +16,6 @@ from google.genai import types
 # CONFIG
 # =========================
 import os
-from dotenv import load_dotenv
-
-# Load environment variables
-load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 MODEL_NAME = "gemini-2.5-flash"
